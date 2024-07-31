6f9dfb6ab71528738640dd8f825f4f2341d02507
Karman1600 <karmanpartapsingh.sidhu@edu.sait.ca>
2024-07-31 14:15:53 -0600

    Commiting backend

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,3 +1,4 @@
+import { Login } from '@/actions/authServer'
 import Head from 'next/head'
 import Link from 'next/link'
 
@@ -12,7 +13,7 @@ export default function SignIn() {
           <div>
             <h2 className="text-2xl font-bold text-center text-blue-600">Sign in to your account</h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" action={Login}>
             <div className="space-y-4 rounded-md shadow-sm">
               <div>
                 <label htmlFor="email" className="sr-only">Email address</label>
@@ -20,7 +21,6 @@ export default function SignIn() {
                   id="email"
                   name="email"
                   type="email"
-                  autoComplete="email"
                   required
                   className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   placeholder="Email address"
@@ -32,7 +32,6 @@ export default function SignIn() {
                   id="password"
                   name="password"
                   type="password"
-                  autoComplete="current-password"
                   required
                   className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   placeholder="Password"
